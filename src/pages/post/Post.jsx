42978af07a595596c820c1b1e1cace4ec11a74f0
Ramziddinergashaliyev <155714547+Ramziddinergashaliyev@users.noreply.github.com>
2026-04-07 12:35:39 +0500

    add

--- src/pages/post/Post.jsx
+++ src/pages/post/Post.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import "./post.scss"
 import { POSTDATA } from '../../static'
 
 const Post = () => {
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [])
+
   return (
     <div className='post container'>
       <h1 className='post-title'>Поставщики</h1>
